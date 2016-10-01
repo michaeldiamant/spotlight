f042ee916e0328232c083a137ac47c41687d07be
mdiamant <michaeldiamant@users.noreply.github.com>
2016-10-01 18:22:55 -0400

    Use bootstrap stylings

--- spotlight-web-client/webpack.config.js
+++ spotlight-web-client/webpack.config.js
@@ -15,8 +15,20 @@ module.exports = {
         loader: 'babel'
       },
       { 
-        test: /\.css$/, 
+        test: /\.css$/,
         loader: "style-loader!css-loader" 
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   }
