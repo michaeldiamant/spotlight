60a48fdc6f9ed19a7c38f68d12a87b6fa1e3fb61
mdiamant <michaeldiamant@users.noreply.github.com>
2016-10-17 19:03:04 -0400

    Externalize OrderBook

---
+++ spotlight-web-client/src/app/OrderBook.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+import BookSide from './BookSide';
+
+class OrderBook extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-4">
+            <p className="text-center"><strong>Order Book</strong></p>
+          </div>
+        </div>  
+        <div className="row">
+          <div className="col-md-2">
+            <BookSide name="Bids" />
+          </div>
+          <div className="col-md-2">
+            <BookSide name="Offers" orders={this.props.offers} /> 
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default OrderBook;
