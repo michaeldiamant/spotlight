5a4561bfbe4dda9138f15b418ea378dc920c3aa9
mdiamant <diamant.michael@gmail.com>
2016-11-03 19:59:12 -0400

    Replace var with const usage

--- spotlight-web-client/__test__/app/PriceLevel.test.js
+++ spotlight-web-client/__test__/app/PriceLevel.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import PriceLevel from '../../src/app/PriceLevel';
 
 test('price level renders price and size', () => {
-  var rendered = renderer.create(
+  const rendered = renderer.create(
     <PriceLevel price='2.01' size='1' />
   );
   expect(rendered.toJSON()).toMatchSnapshot();
